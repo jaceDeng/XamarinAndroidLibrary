063c29ba537df75b362d5d856131840d87b5e283
deng <mingx@qq.com>
2021-11-23 17:22:36 +0800

    注释掉

--- BaiduMapSDK_Map/BaiduMapSDK_Map/Additions/ItemizedOverlay.cs
+++ BaiduMapSDK_Map/BaiduMapSDK_Map/Additions/ItemizedOverlay.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace BaiduMapSDK_Map.Additions
 {
-    public partial class ItemizedOverlay : global::Com.Baidu.Platform.Comapi.Map.Overlay, global::Java.Util.IComparator
-    {
-        public int Compare(Java.Lang.Object o1, Java.Lang.Object o2)
-        {
-            return Compare((Java.Lang.Integer)o1, (Java.Lang.Integer)o2);
+    //public partial class ItemizedOverlay : global::Com.Baidu.Platform.Comapi.Map.Overlay, global::Java.Util.IComparator
+    //{
+    //    public int Compare(Java.Lang.Object o1, Java.Lang.Object o2)
+    //    {
+    //        return Compare((Java.Lang.Integer)o1, (Java.Lang.Integer)o2);
                  
-        }
-    }
+    //    }
+    //}
 }
