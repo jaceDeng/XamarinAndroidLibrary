0be4a4e60492dcd99eb553f5d0371a8f77a196bc
JSON <micro.man@live.cn>
2022-01-09 00:36:41 +0800

    x5 绑定最新的 44136

--- AppTBS/AppTBS/AppTBS.Android/MApplication.cs
+++ AppTBS/AppTBS/AppTBS.Android/MApplication.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Com.Tencent.Smtt.Export.External;
+using Com.Tencent.Smtt.Sdk;
 
 namespace AppTBS.Droid
 {
@@ -21,6 +23,12 @@ namespace AppTBS.Droid
         }
         public override void OnCreate()
         {
+            var  map = new Dictionary<string,Java.Lang.Object>();
+            map.Add(TbsCoreSettings.TbsSettingsUseSpeedyClassloader, true);
+            map.Add(TbsCoreSettings.TbsSettingsUseDexloaderService, true);
+            QbSdk.InitTbsSettings(map);
+            QbSdk.DownloadWithoutWifi=(true);
+            TbsDownloader.StartDownload(this);
             Com.Tencent.Smtt.Sdk.QbSdk.InitX5Environment(this,null);
             //Com.Tencent.Bugly.Bugly.Init(this.ApplicationContext, APP_ID, false);
             base.OnCreate();
