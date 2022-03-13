a374ad243107c046fdfcd3cedcf0913904a08d5c
JSON <micro.man@live.cn>
2022-03-13 20:57:34 +0800

    绑定rtmp 推送

---
+++ rtmp-rtsp-stream-client/Rtmp.Droid/Additions/UserControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Com.Pedro.Rtmp.Rtmp.Message.Control
+{
+
+    public sealed partial class UserControl : global::Com.Pedro.Rtmp.Rtmp.Message.RtmpMessage
+    {
+        /// <summary>
+        /// 为了和java的api 保持一致 可以用GetTypeEnum代替
+        /// </summary>
+        /// <returns></returns>
+        public Type getType()
+        {
+
+            return GetTypeEnum();
+        }
+
+         
+
+    }
+}
